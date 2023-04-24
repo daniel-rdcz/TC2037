8e54de8d1ef75a6c2241367d5ca32a5aeb2b9dd1
daniel-rdcz <111093272+daniel-rdcz@users.noreply.github.com>
2023-04-24 09:54:54 -0600

    Update 02_arithmetic_dfa.rkt

--- 02_arithmetic_dfa.rkt
+++ 02_arithmetic_dfa.rkt
@@ -43,13 +43,14 @@ Daniel Rodriguez
     ([chars (string->list strng)]
      ; Get the initial state of the DFA
      [state (dfa-initial dfa-to-evaluate)]
+     {current-token '()}
      ; Then return list with all the tokens found
      [tokens '()])
     (cond
       ; When the list of chars if over, check if the final state is acceptable
       [(empty? chars)
        (if (member state (dfa-accept dfa-to-evaluate))
-           (reverse (filter (lambda (x) (not (eq? x 'spa))) (cons state tokens)))
+           (reverse (filter (lambda (x) (not (eq? x 'spa))) (cons (list (list->string (reverse current-token)) state) tokens)))
            'invalid)]
       [else
        (let-values
@@ -57,8 +58,10 @@ Daniel Rodriguez
        ([(new-state found) ((dfa-func dfa-to-evaluate) state (car chars))])
        (loop (cdr chars)
              new-state
+             (if found (cons(car chars) current-token)
+                 (list(if (and found (not (eq? found 'spa))) '() (car chars))))
              ; The new list of tokens
-             (if (and found (not (eq? found 'spa))) (cons found tokens) tokens)))])))
+             (if (and found (not (eq? found 'spa))) (cons (list (list->string (reverse current-token)) found) tokens) tokens)))])))
 
 
 ; 567.2 - 341
@@ -78,58 +81,58 @@ Daniel Rodriguez
   (case state
     ['start (cond
        [(char-numeric? char) (values 'int (string char))]
-       [(eq? char #\space) (values 'spa (string char))]
-       [(or (eq? char #\+) (eq? char #\-)) (values 'sign (string char))]
-       [(char-alphabetic? char) (values 'var (string char))]
-       [(eq? char #\_) (values 'var (string char))]
-       [(eq? char #\() (values 'opening_parenthesis (string char))]
-       [else (values 'inv (string char))])]
+       [(eq? char #\space) (values 'spa #f)]
+       [(or (eq? char #\+) (eq? char #\-)) (values 'sign #f)]
+       [(char-alphabetic? char) (values 'var #f)]
+       [(eq? char #\_) (values 'var #f)]
+       [(eq? char #\() (values 'opening_parenthesis #f)]
+       [else (values 'inv #f)])]
     ['sign (cond
-       [(char-numeric? char) (values 'int (string char))]
-       [else (values 'inv (string char))])]
+       [(char-numeric? char) (values 'int #f)]
+       [else (values 'inv #f)])]
     ['int (cond
        [(char-numeric? char) (values 'int (string char))]
-       [(eq? char #\.) (values 'dot  (string char))]
-       [(or (eq? char #\e) (eq? char #\E)) (values 'e (string char))]
+       [(eq? char #\.) (values 'dot  #f)]
+       [(or (eq? char #\e) (eq? char #\E)) (values 'e #f)]
        [(char-operator? char) (values 'op 'int)]
        [(eq? char #\space) (values 'spa 'int)]
        [(eq? char #\() (values 'opening_parenthesis 'int)]
        [(eq? char #\)) (values 'closing_parenthesis 'int)]
-       [else (values 'inv (string char))])]
+       [else (values 'inv #f)])]
     ['dot (cond
-       [(char-numeric? char) (values 'float (string char))]
-       [else (values 'inv (string char))])]
+       [(char-numeric? char) (values 'float #f)]
+       [else (values 'inv #f)])]
     ['float (cond
-       [(char-numeric? char) (values 'float (string char))]
-       [(or (eq? char #\e) (eq? char #\E)) (values 'e (string char))]
+       [(char-numeric? char) (values 'float #f)]
+       [(or (eq? char #\e) (eq? char #\E)) (values 'e #f)]
        [(char-operator? char) (values 'op 'float)]
        [(eq? char #\space) (values 'spa 'float)]
        [(eq? char #\() (values 'opening_parenthesis 'float)]
        [(eq? char #\)) (values 'closing_parenthesis 'float)]
-       [else (values 'inv (string char))])]
+       [else (values 'inv #f)])]
     ['e (cond
-       [(char-numeric? char) (values 'exp (string char))]
-       [(or (eq? char #\+) (eq? char #\-)) (values 'e_sign (string char))]
-       [else (values 'inv (string char))])]
+       [(char-numeric? char) (values 'exp #f)]
+       [(or (eq? char #\+) (eq? char #\-)) (values 'e_sign #f)]
+       [else (values 'inv #f)])]
     ['e_sign (cond
-       [(char-numeric? char) (values 'exp (string char))]
-       [else (values 'inv (string char))])]
+       [(char-numeric? char) (values 'exp #f)]
+       [else (values 'inv #f)])]
     ['exp (cond
-       [(char-numeric? char) (values 'exp (string char))]
+       [(char-numeric? char) (values 'exp #f)]
        [(char-operator? char) (values 'op 'exp)]
        [(eq? char #\space) (values 'spa 'exp)]
        [(eq? char #\() (values 'opening_parenthesis 'exp)]
        [(eq? char #\)) (values 'closing_parenthesis 'exp)]
-       [else (values 'inv (string char))])]
+       [else (values 'inv #f)])]
     ['var (cond
-       [(char-alphabetic? char) (values 'var (string char))]
-       [(char-numeric? char) (values 'var (string char))]
-       [(eq? char #\_) (values 'var (string char))]
+       [(char-alphabetic? char) (values 'var #f)]
+       [(char-numeric? char) (values 'var #f)]
+       [(eq? char #\_) (values 'var #f)]
        [(char-operator? char) (values 'op 'var)]
        [(eq? char #\space) (values 'spa 'var)]
        [(eq? char #\() (values 'opening_parenthesis 'var)]
        [(eq? char #\)) (values 'closing_parenthesis 'var)]
-       [else (values 'inv (string char))])]
+       [else (values 'inv #f)])]
     ['op (cond
        [(char-numeric? char) (values 'int 'op)]
        [(or (eq? char #\+) (eq? char #\-)) (values 'sign 'op)]
@@ -138,19 +141,19 @@ Daniel Rodriguez
        [(eq? char #\space) (values 'op_spa 'op)]
        [(eq? char #\() (values 'opening_parenthesis 'op)]
        [(eq? char #\)) (values 'closing_parenthesis 'op)]
-       [else (values 'inv (string char))])]
+       [else (values 'inv #f)])]
      ['spa (cond
-       [(char-operator? char) (values 'op (string char))]
-       [(eq? char #\space) (values 'spa (string char))]
-       [(char-numeric? char) (values 'int (string char))]
-       [else (values 'inv (string char))])]
+       [(char-operator? char) (values 'op #f)]
+       [(eq? char #\space) (values 'spa #f)]
+       [(char-numeric? char) (values 'int #f)]
+       [else (values 'inv #f)])]
     ['op_spa (cond
-       [(char-numeric? char) (values 'int (string char))]
-       [(or (eq? char #\+) (eq? char #\-)) (values 'sign (string char))]
-       [(char-alphabetic? char) (values 'var (string char))]
-       [(eq? char #\_) (values 'var (string char))]
-       [(eq? char #\space) (values 'op_spa (string char))]
-       [else (values 'inv (string char))])]
+       [(char-numeric? char) (values 'int #f)]
+       [(or (eq? char #\+) (eq? char #\-)) (values 'sign #f)]
+       [(char-alphabetic? char) (values 'var #f)]
+       [(eq? char #\_) (values 'var #f)]
+       [(eq? char #\space) (values 'op_spa #f)]
+       [else (values 'inv #f)])]
     ['opening_parenthesis (cond
        [(char-numeric? char) (values 'int 'opening_parenthesis)]
        [(or (eq? char #\+) (eq? char #\-)) (values 'sign 'opening_parenthesis)]
@@ -158,7 +161,7 @@ Daniel Rodriguez
        [(eq? char #\() (values 'opening_parenthesis 'opening_paretnhesis)]
        [(eq? char #\)) (values 'closing_parenthesis 'opening_parenthesis)]
        [(eq? char #\_) (values 'var 'opening_parenthesis)]
-       [else (values 'inv (string char))])]
+       [else (values 'inv #f)])]
     ['closing_parenthesis (cond
        [(char-numeric? char) (values 'int 'closing_parenthesis)]
        [(or (eq? char #\+) (eq? char #\-)) (values 'sign 'closing_parenthesis)]
@@ -167,8 +170,8 @@ Daniel Rodriguez
        [(eq? char #\)) (values 'closing_parenthesis 'closing_parenthesis)]
        [(eq? char #\space) (values 'spa 'closing_parenthesis)]
        [(eq? char #\_) (values 'var 'closing_parenthesis)]
-       [else (values 'inv (string char))])]
+       [else (values 'inv #f)])]
     ))
 
 
-(arithmetic-lexer "22 - 7")
+(arithmetic-lexer "22")
